44dc1f470e0bca2d4bb921d86ffc247e6a684bb2
贾应许 <iphone_project@163.com>
2017-02-06 10:10:36 +0800

    Initial Commit

---
+++ iphoneBlue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iphoneBlue
+//
+//  Created by ydz on 17/2/6.
+//  Copyright © 2017年 JYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
